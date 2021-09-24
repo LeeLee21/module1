ae10ea737892585cdff2e229da494372c83d180e
LeeLee <davydiukillia@gmail.com>
2021-09-24 17:50:16 +0300

    9 Added database table

---
+++ web/sites/default/files/php/twig/614de5129c104_cat-theme.html.twig_WD7tdV9jlbZdFenICgfwX_Wo_/e573s8VxWCYqtOOmqwg9FmzBQ74UZfADw7IOxw-dqLo.php
@@ -0,0 +1,109 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/custom/leelee/templates/cat-theme.html.twig */
+class __TwigTemplate_f6ce4d1f58736995bffe2cc9fa11cb4d1cfbaefc00127803cd13f9fb17a9e34d extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->attachLibrary("leelee/leelee-style"), "html", null, true);
+        echo "
+
+<div class=\"cats-content-wrapper\">
+  <div class=\"form\">
+    <div class=\"message-status\"></div>
+    ";
+        // line 6
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["form"] ?? null), 6, $this->source), "html", null, true);
+        echo "
+  </div>
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/custom/leelee/templates/cat-theme.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  47 => 6,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{{ attach_library('leelee/leelee-style') }}
+
+<div class=\"cats-content-wrapper\">
+  <div class=\"form\">
+    <div class=\"message-status\"></div>
+    {{ form }}
+  </div>
+</div>
+", "modules/custom/leelee/templates/cat-theme.html.twig", "/var/www/web/modules/custom/leelee/templates/cat-theme.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 1);
+        static $functions = array("attach_library" => 1);
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                ['attach_library']
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
